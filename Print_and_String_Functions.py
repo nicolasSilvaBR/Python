f8a6a38b7fae14090ac13258e02899b518a5ca16
Nicolas Antonio da Silva <78442901+nicolasSilvaBR@users.noreply.github.com>
2022-04-27 07:19:26 +0100

    Add files via upload

---
+++ Print_and_String_Functions.py
@@ -0,0 +1,49 @@
+# Print_and_String_Functions
+
+print("print('what to print')")
+
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
+
+# /n new line
+print('Exemple\n Another')
+
+# f string
+print(f'When you want to Print a number : {Number}')
+
+# Priting Long Texts using triple coutes '''
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# priting one Char from a String
+a = "Hello, World!"
+print(a[1]) #Output will be 'e'
+
+# Get the characters from position 2 to position 5 (not included):
+b = "Hello, World!"
+print(b[2:5])
+
+# Get the characters from the start to position 5 (not included):
+b = "Hello, World!"
+print(b[:5])
+
+# Upper 
+# The upper() method returns the string in upper case:
+a = "Hello, World!"
+print(a.upper())
+
+# Lower
+# The lower() method returns the string in lower case:
+a = "Hello, World!"
+print(a.lower())
+
+# Strip 
+# The strip() method removes any whitespace from the beginning or the end:
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+# https://www.w3schools.com/python/python_strings.asp
+
